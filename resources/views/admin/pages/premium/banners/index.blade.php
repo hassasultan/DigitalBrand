c9c61770bc9f23518774882bb5691078caac9ec4
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2023-05-04 16:53:05 +0500

    Banner Form Complete & 1 new field added in seller create form

--- resources/views/admin/pages/premium/banners/index.blade.php
+++ resources/views/admin/pages/premium/banners/index.blade.php
@@ -23,7 +23,7 @@
                         <div class="datatable-header">
                             <div class="row align-items-center mb-3">
                                 <div class="col-12 col-lg-auto mb-3 mb-lg-0">
-                                    <a href="ecommerce-category-form.html" class="btn btn-primary btn-md font-weight-semibold btn-py-2 px-4">+ Add Banner</a>
+                                    <a href="/banners/form" class="btn btn-primary btn-md font-weight-semibold btn-py-2 px-4">+ Add Banner</a>
                                 </div>
                                 <div class="col-8 col-lg-auto ms-auto ml-auto mb-3 mb-lg-0">
                                     <div class="d-flex align-items-lg-center flex-column flex-lg-row">
