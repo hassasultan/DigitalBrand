abf8cb2857d3b2a147f8aa315fb04127f41c95a1
Hassan Sultan <h.hsultan277@gmail.com>
2023-06-26 21:11:10 +0500

    hassan

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -165,7 +165,7 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        dd($request->all());
+        return $request->all();
         try
         {
             $request->validate([
