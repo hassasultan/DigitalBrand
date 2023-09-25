210222594a608d803ae21ef9f66b3ee8672fbbda
Hassan Sultan <h.hsultan277@gmail.com>
2023-09-26 00:02:56 +0500

    pushed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -47,6 +47,7 @@ class PostController extends Controller
     public function create_offer_api(Request $request)
     {
         try {
+            dd($request->all());
             $this->validate($request, [
                 'banner' => 'required|image|mimes:jpg,bmp,png,webp|max:2048',
                 'title' => 'required',
