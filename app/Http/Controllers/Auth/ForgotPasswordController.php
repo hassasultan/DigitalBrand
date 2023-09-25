39a19e800e293225fce61bb1bd1b4e3627309dc5
Hassan Sultan <h.hsultan277@gmail.com>
2023-09-26 02:52:33 +0500

    pushed

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Password;
-
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 class ForgotPasswordController extends Controller
 {
     /*
@@ -23,12 +26,30 @@ class ForgotPasswordController extends Controller
     use SendsPasswordResetEmails;
     public function forgot(Request $request)
     {
-        $this->validate($request, ['email' => 'required|email']);
-
-        $response = Password::sendResetLink($request->only('email'));
-
-        return $response == Password::RESET_LINK_SENT
-            ? response()->json(['message' => 'Password reset link sent'], 200)
-            : response()->json(['error' => 'Unable to send reset link'], 400);
+        $input = $request->all();
+        $rules = array(
+            'email' => "required|email",
+        );
+        $validator = Validator::make($input, $rules);
+        if ($validator->fails()) {
+            $arr = array("status" => 400, "message" => $validator->errors()->first(), "data" => array());
+        } else {
+            try {
+                $response = Password::sendResetLink($request->only('email'), function (Message $message) {
+                    $message->subject($this->getEmailSubject());
+                });
+                switch ($response) {
+                    case Password::RESET_LINK_SENT:
+                        return \Response::json(array("status" => 200, "message" => trans($response), "data" => array()));
+                    case Password::INVALID_USER:
+                        return \Response::json(array("status" => 400, "message" => trans($response), "data" => array()));
+                }
+            } catch (\Swift_TransportException $ex) {
+                $arr = array("status" => 400, "message" => $ex->getMessage(), "data" => []);
+            } catch (Exception $ex) {
+                $arr = array("status" => 400, "message" => $ex->getMessage(), "data" => []);
+            }
+        }
+        return \Response::json($arr);
     }
 }
